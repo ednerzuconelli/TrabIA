4dca1796a4a9088a2070381360eb7f876d864a72
ednerzuconelli <ednerzuconelli@gmail.com>
2016-06-02 10:12:01 -0300

    manter somente classes

--- iss/src/main/java/br/com/uem/iss/trabalho/servico/PessoaServico.java
+++
@@ -1,9 +0,0 @@
-package br.com.uem.iss.trabalho.servico;
-
-import br.com.uem.iss.trabalho.modelo.Pessoa;
-
-public interface PessoaServico {
-
-	public Pessoa getByCod(Integer cod);
-	public void save(Pessoa pessoa);
-}
