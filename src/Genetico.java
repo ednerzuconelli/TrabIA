d168603ea4b64377ae3ff9d75a6cdefb373c7d1e
ednerzuconelli <ednerzuconelli@gmail.com>
2016-06-12 10:29:18 -0300

    imports

---
+++ src/Genetico.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Genetico {
+
+	
+	private List<Individuo> populacao;
+	
+	public Genetico(){
+		populacao = new ArrayList<Individuo>();
+	}
+	
+	public void inicializar(){
+		Individuo individuo = new Individuo();
+		for(int i=0;i<10;i++){
+			
+		}
+	}
+}
