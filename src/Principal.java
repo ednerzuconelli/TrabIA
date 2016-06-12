77802434508bd0da3e68cbbd9b8f9b9959995996
ednerzuconelli <ednerzuconelli@gmail.com>
2016-06-12 10:54:38 -0300

    compara

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,26 @@
+import java.util.List;
 
 public class Principal {
-	public static void main(){
+	public static void main(String[] args){
+		
+	}
+	private Individuo geraGanhador(Individuo primeiro, Individuo segundo){
+		Individuo individuoGanhador = geraMercado();
+		if (individuoGanhador.equals(primeiro))
+			System.out.println("Individuo primeiro ganhador");
+		if (individuoGanhador.equals(segundo))
+			System.out.println("Individuo segundo ganhador");
+		return individuoGanhador;
+		
+	}
+	private Individuo geraMercado(){
+		Individuo individuo = null;
+		individuo.addCromossomo("ABCDC");
+		List<String> cromossomo=null;
+		cromossomo.add("E");
+		individuo.setCromossomo(cromossomo);
+		individuo.setValor(15);
+		return individuo;
 		
 	}
 }
